8172858dc988774551eddf87aa8ae0e265908c87
Sarah Strohkorb <sarah.strohkorb@gmail.com>
2014-02-25 11:37:11 -0500

    HW4 grading comments

--- hw4/random_art.py
+++ hw4/random_art.py
@@ -7,6 +7,7 @@ Created on Tue Feb 11 11:34:57 2014
 
 # you do not have to use these particular modules, but they may help
 from random import randint
+from math import *          ## SS: I added this line - don't forget to add the math library dependency!
 import Image
 
 #need to include prod(a,b)=ab, cos_pi(a)=cos(pi*a), sin '', x(a,b)=a, y '' + 2 others
@@ -70,6 +71,9 @@ def build_random_function(min_depth, max_depth):
             return [z,build_random_function(min_depth-1,max_depth-1),build_random_function(min_depth-1,max_depth-1)]
             
 
+## SS: Passed my tests :)
+## SS: for this function, you might make use of 'elif' statements, even though the functionailty
+##     is the same, stylistically, it's preferable
 def evaluate_random_function(f, x, y):
     """Takes as input a function constructed by nested lists, x, and y and returns 
     a value between 0 and 1 that evaluates it."""
@@ -94,6 +98,8 @@ def evaluate_random_function(f, x, y):
     print f[0]
     #unit test: ['prod', ['prod', ['x'], ['y']], ['prod', ['x'], ['y']]] should return (xy)^2
             
+## SS: Passed my tests :)
+## SS: maybe use some more descriptive variable names here
 def remap_interval(val, input_interval_start, input_interval_end, output_interval_start, output_interval_end):
     """ Maps the input value that is in the interval [input_interval_start, input_interval_end]
         to the output interval [output_interval_start, output_interval_end].  The mapping
@@ -110,8 +116,8 @@ def generate_image():
     ysize = 350 
     im = Image.new("RGB",(xsize,ysize))
     pix = im.load()
-    min_args = 16
-    max_args = 18
+    min_args = 4
+    max_args = 5
     funcR=build_random_function(min_args,max_args)
     funcG=build_random_function(min_args,max_args)
     funcB=build_random_function(min_args,max_args)
@@ -128,6 +134,10 @@ def generate_image():
             pix[i,j]=(mappedR,mappedG,mappedB)
     im.save('ex7.png')            
             
+generate_image()
+
+## SS: Hey, great job! This is flawless in functionality (or so I can tell), and you have good
+##     documentation, which I really appreciate. 
 
 
 

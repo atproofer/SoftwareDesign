9e84d6854c3d60e3c831dcb981216f4cc9a8df4c
Michael Bocamazo <atproofer@mbocamazo.(none)>
2014-03-10 02:31:48 -0400

    commiting hw6

---
+++ hw6/PygameTD.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  6 23:40:46 2014
+
+@author: atproofer - Michael Bocamazo
+"""
+
+import pygame
+from pygame.locals import *
+import random
+import math
+import time
+
+class TDModel:
+    """Encodes game"""
